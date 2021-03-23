bfb8dc86e14a4b9b5da969c128d1cd35be9d4447
Jefferson <jeffersonbraster@gmail.com>
2021-03-23 14:27:12 -0300

    fazendo login no github com next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 export function SignInButton() {
-  const isUserLogegrIn = true;
+  const [session] = useSession();
 
-  return isUserLogegrIn ? (
-    <button className={styles.SignInButton} type="button">
+  return session ? (
+    <button
+      className={styles.SignInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d301" />
-      Jefferson Brandão
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.SignInButton} type="button">
+    <button
+      className={styles.SignInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubcribeButton";
 import { stripe } from "../services/stripe";
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IXuqxD6Uh4ypEg9qhBQAAtC");
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 
   return {
+    revalidate: 60 * 60 * 24, //24hs
     props: {
       product,
     },
