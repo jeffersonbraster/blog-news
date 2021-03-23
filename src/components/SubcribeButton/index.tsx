14d4a6622d7a9a42dc1f19f1d19f9898522342f3
Jefferson <jeffersonbraster@gmail.com>
2021-03-23 17:02:25 -0300

    redirecionando para o checkout do stripe

--- src/components/SubcribeButton/index.tsx
+++ src/components/SubcribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signin } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 type SubscriptionProps = {
@@ -5,8 +8,35 @@ type SubscriptionProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscriptionProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    //verifica se está logado
+    if (!session) {
+      signin("github");
+      return;
+    }
+
+    //cria o checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribe}>
+    <button
+      type="button"
+      className={styles.subscribe}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
