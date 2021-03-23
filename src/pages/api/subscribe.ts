ce237ef6b8e51c9b9027257fc8d3522909b7bf82
Jefferson <jeffersonbraster@gmail.com>
2021-03-23 17:31:41 -0300

    verificando duplicidade no cadastro do usuário no strapi

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,47 @@
+import { query as q } from "faunadb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async function (req: NextApiRequest, response: NextApiResponse) {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      name: session.user.name,
-    });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        name: session.user.name,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const checkout = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1IXuqxD6Uh4ypEg9qhBQAAtC", quantity: 1 }],
